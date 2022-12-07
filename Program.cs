1be0e1ee66c02f576d25e029d1690c63b7debaeb
vladimir golovin <tkomplect@mail.ru>
2022-12-07 18:34:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//                                            ДОМАШНЕЕ ЗАДАНИЕ к урок1 знакомство с C#.
+
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//Console.WriteLine("Введите число a");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число b");
+//int b = int.Parse(Console.ReadLine());
+//int max = a;
+//int min = b;
+//if (a > max) max = a;
+//if (b > max) max = b;
+//if (a < min) min = a;
+//if (b < min) min = b;
+//Console.Write("-> max =");
+//Console.WriteLine(max);
+//Console.Write("-> min =");
+//Console.WriteLine(min);
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+//Console.WriteLine("Введите число a");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число b");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите число c");
+//int c =int.Parse(Console.ReadLine());
+//int max = a;
+//if (a > max) max = a;
+//if (b > max) max = b;
+//if (c > max) max = c;
+//Console.Write("Максимальное число ->");
+//Console.WriteLine(max);
+
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+//Console.WriteLine("Введите число");
+//int n = int.Parse(Console.ReadLine());
+//if (n % 2 == 0)
+//      Console.Write("Да");
+//if (n % 2 != 0)
+//      Console.Write("Нет");
+
+
+
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//Console.WriteLine("Введите число");
+//int N = int.Parse(Console.ReadLine());
+//for (int i = 1; i <= N; i++) 
+//   if (i % 2 == 0)
+//            Console.WriteLine(i);
+
+
